089b596e5fc87180b65dcabfab415ffc4d00fd5e
HewanHagos <yemanezgreathagos@gmail.com>
2023-07-20 12:47:15 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle
+ * @size: size of the triagonal
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (k = size - i; k >= 1; k--)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
+
+
