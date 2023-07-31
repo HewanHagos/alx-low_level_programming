0862338e9456a83535331b1f9e934a95d061f6ac
HewanHagos <yemanezgreathagos@gmail.com>
2023-07-31 07:16:56 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * _strcht - locates a character in a string
- * @s: The string to be searched
+ * _strchr - locates a charachter in a string
+ * @s: the string to be searched
  * @c: the character to be located
- * Return: Returns a pointer to the first occurrence of the
- * character c in the string s, or NULL if the character is not found
+ * Return: a pointer to the first occurrence of
+ * the character c in the string s,
+ * or NULL if the character is not found
  */
 
 char *_strchr(char *s, char c)
