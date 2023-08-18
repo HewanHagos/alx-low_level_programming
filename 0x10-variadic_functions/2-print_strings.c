e22d99808954a9a74877c272054769cb34d9e92e
HewanHagos <yemanezgreathagos@gmail.com>
2023-08-17 18:04:22 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-print_strings.c -o c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,15 +17,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(list, char *);
-		if (!str)
-			str = "nil";
-		if (!separator)
-			printf("%s", str);
-		else if (separator && i != 0)
-			printf("%s", str);
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+		if (str == 0)
+			printf("(nil)");
+
 		else
-			printf("%s %s", separator, str);
+			printf("%s", str);
 	}
 	printf("\n");
+
 	va_end(list);
 }
